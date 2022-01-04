cebc08e645a077b341b0b964dd5d63059cedc703
Matt Parisien <matthewparisien4@gmail.com>
2022-01-04 11:08:22 -0500

    Add mp4 option to gallery

--- src/components/ImageGallery/GalleryItem.js
+++ src/components/ImageGallery/GalleryItem.js
@@ -4,6 +4,24 @@ import { useEffect } from "react/cjs/react.development";
 import { useIntersect } from "../../helpers/hooks/useIntersect";
 
 function GalleryItem(props, ref) {
+
+	
+	const images = props.image && (
+		<img
+			className='image-gallery__item__image'
+			src={`${process.env.REACT_APP_STRAPI_URL + props.image}`}
+			alt='grid-image'
+		></img>
+	);
+
+	const videos = props.video && (
+		<video autoPlay loop muted playsInline width={"100%"}>
+			<source src={`${process.env.REACT_APP_STRAPI_URL + props.video}`}></source>
+		</video>
+	)
+
+	
+
 	return (
 		<div
 			className={`image-gallery__item ${props.isVisible ? "is-visible" : ""}`}
@@ -13,11 +31,8 @@ function GalleryItem(props, ref) {
 				className='image-gallery__item__image-wrapper -isFull'
 				y={[-30, 20]}
 			>
-				<img
-					className='image-gallery__item__image'
-					src={`${process.env.REACT_APP_STRAPI_URL + props.image}`}
-					alt='grid-image'
-				></img>
+				{images}
+				{videos}
 			</Parallax>
 		</div>
 	);
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -12,41 +12,55 @@ function ImageGallery(props) {
 
 	const addToRefs = el => {
 		if (imageRefs.current && !imageRefs.current.includes(el)) {
-			
 			imageRefs.current.push(el);
 		}
 	};
 
-
-
 	useEffect(() => {
 		if (imageRefs.current) {
-			console.log(imageRefs.current)
-			const handleIntersection = (entries) => {
+			console.log(imageRefs.current);
+			const handleIntersection = entries => {
 				entries.forEach(entry => {
-					setVisible(entry.isIntersecting)
-				})
-			}
+					setVisible(entry.isIntersecting);
+				});
+			};
 
-			const observer = new IntersectionObserver(handleIntersection, { threshold: 1 })
+			const observer = new IntersectionObserver(handleIntersection, {
+				threshold: 1,
+			});
 
 			imageRefs.current.forEach(image => {
 				observer.observe(image);
-			})
-
+			});
 		}
-	}, [imageRefs, isVisible])
-
+	}, [imageRefs, isVisible]);
 
 	const images =
 		data &&
 		data.map(post => {
+			console.log("post", post);
 			return post.attributes.Photo.data.map((image, index) => {
-				return <GalleryItem image={image.attributes.url} key={index} ref={addToRefs} isVisible={isVisible}/>;
+				return (
+					<GalleryItem
+						image={
+							image.attributes.ext !== ".mp4" ? image.attributes.url : null
+						}
+						video={
+							image.attributes.ext === ".mp4" ? image.attributes.url : null
+						}
+						key={index}
+						ref={addToRefs}
+						isVisible={isVisible}
+					/>
+				);
 			});
 		});
 
-	return <div ref={containerRef} className='image-gallery-container -isFull'>{images}</div>;
+	return (
+		<div ref={containerRef} className='image-gallery-container -isFull'>
+			{images}
+		</div>
+	);
 }
 
 export default ImageGallery;
