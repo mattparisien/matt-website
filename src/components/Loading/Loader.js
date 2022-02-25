c6ffc77babf094a121142fd27290b448de4c3a8f
Matt Parisien <matthewparisien4@gmail.com>
2022-02-25 09:04:24 -0500

    Add loader animaitno

--- src/components/Loading/Loader.js
+++ src/components/Loading/Loader.js
@@ -15,21 +15,108 @@ function Loader() {
 		zIndex: 9999,
 		backgroundColor: theme.colors.dark,
 		color: theme.colors.light,
-    fontFamily: 'Haas',
-    display: 'flex',
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-    fontSize: "15vw"
-
-
+		fontFamily: "Haas",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+		flexDirection: "column",
+		fontSize: "15vw",
+    display: "none"
 	};
+
+	const wordStyle = {
+		opacity: 0,
+	};
+
+	const word1 = useRef(null);
+	const word2 = useRef(null);
+	const word3 = useRef(null);
+	const fadeInTimeline = useRef(gsap.timeline());
+	const fadeOutTimeline = useRef(gsap.timeline());
+	const masterTimeline = useRef(gsap.timeline());
+
+	useEffect(() => {
+		if (word1.current && word2.current && word3.current) {
+			const speed = 0.2;
+			const ease = "linear.easeNone";
+
+			const fadeIn = () => {
+				fadeInTimeline.current
+					.to(
+						word1.current,
+						{
+							opacity: 1,
+							duration: speed,
+							ease: ease,
+						},
+						0
+					)
+					.to(
+						word2.current,
+						{
+							opacity: 1,
+							duration: speed,
+							ease: ease,
+						},
+						speed
+					)
+					.to(
+						word3.current,
+						{
+							opacity: 1,
+							duration: speed,
+							ease: ease,
+						},
+						speed * 2
+					);
+				return fadeInTimeline.current;
+			};
+
+			const fadeOut = () => {
+				fadeOutTimeline.current
+					.to(
+						word1.current,
+						{
+							opacity: 0,
+							ease: ease,
+						},
+						speed
+					)
+					.to(
+						word2.current,
+						{
+							opacity: 0,
+							ease: ease,
+						},
+						speed * 2
+					)
+					.to(
+						word3.current,
+						{
+							opacity: 0,
+							ease: ease,
+						},
+						speed * 3
+					);
+
+				return fadeOutTimeline.current;
+			};
+
+			masterTimeline.current.add(fadeIn()).add(fadeOut(), "-=0.5");
+		}
+	}, [word1, word2, word3]);
 
 	return (
 		<Box sx={loaderStyle} component='div'>
-			<span>Work hard</span>
-			<span>Work hard</span>
-			<span>Work hard</span>
+			<span ref={word1} style={wordStyle}>
+				Work hard
+			</span>
+			<span ref={word2} style={wordStyle}>
+				Work hard
+			</span>
+			<span ref={word3} style={wordStyle}>
+				Work hard
+			</span>
 		</Box>
 	);
 }
