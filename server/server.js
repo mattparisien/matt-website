008c982cf94dc0b7b8beef927062aba28814e12f
Matt Parisien <matthewparisien4@gmail.com>
2022-02-25 12:59:02 -0500

    Begin building mongo schema

--- server/server.js
+++ server/server.js
@@ -1,8 +1,32 @@
 const express = require("express");
 const PORT = 8080;
 const app = express();
+const router = require("express").Router();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
+//Express config
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static("public"));
+require("dotenv").config();
+
+//Mongo config
+mongoose.connect(
+	process.env.MONGO_URI,
+	() => {
+		console.log("Connected");
+	},
+	e => console.log(e)
+);
+
+//Routing config
+app.use("/api", router);
+
+router.get("/projects", (req, res) => {});
 
 app.listen(PORT, () => {
-  console.log(`Port listening on port ${PORT} 😎😎😎`)
-})
+	console.log(`Server listening on port ${PORT} 😎😎😎`);
+});
