0c614899a55b28c92e7e82acd001290d2f246d5e
Matt Parisien <matthewparisien4@gmail.com>
2022-02-25 14:21:57 -0500

    Build uploader

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const router = require("express").Router();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const upload = require("./multer");
+const cloudinary = require("./cloudinary");
+const fs = require("fs");
 
 //Express config
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +25,30 @@ mongoose.connect(
 	e => console.log(e)
 );
 const Project = require("./db/models/Project");
+
+//Cloudinary config
+app.use("/upload-images", upload.array("image"), async (req, res) => {
+	const uploader = async path => await cloudinary.uploads(path, "Images");
+
+	if (req.method === "POST") {
+		const urls = [];
+		const files = req.files;
+		for (const file of files) {
+			const { path } = file;
+			const newPath = await uploader(path);
+			urls.push(newPath);
+			fs.unlinkSync(path);
+		}
+
+		res
+			.status(200)
+			.json({ message: "images uploaded successfully", data: urls });
+	} else {
+		res.status(405).json({
+			err: `${req.method} method not allowed`,
+		});
+	}
+});
 
 //Routing config
 app.use("/api", router);
