afd58759d12acaa3e3165e43a5b03e681585df5e
Matt Parisien <matthewparisien4@gmail.com>
2022-02-26 10:42:56 -0500

    Add header logo

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,28 +1,24 @@
 import React, { forwardRef, useRef, useState, useEffect } from "react";
 import Section from "../Section";
 import { StyledHeader } from "./styles/StyledHeader";
-import UnorderedList from "../UnorderedList/UnorderedList";
-
+import { Box } from "@mui/system";
+import ContainerFluid from "../Containers/ContainerFluid";
 import { Link } from "react-router-dom";
-import Title from "./Title/Title";
-import Nav from "./Nav";
-import MenuLink from "./Menu/MenuLink";
-import useResize from "../../helpers/hooks/useResize";
 
 function Header(props, ref) {
 	const { currentPath, headerOffset, isMenuActive, isDefaultContentHidden } =
 		props;
 	const [innerHeight, setInnerHeight] = useState(null);
-	const [windowWidth] = useResize();
-
-	const title = useRef(null);
 
 	return (
-		<StyledHeader
-			headerHeight={headerOffset}
-			ref={ref}
-			height={innerHeight}
-		></StyledHeader>
+		<StyledHeader headerHeight={headerOffset} ref={ref} height={innerHeight}>
+			<ContainerFluid>
+				<div className='header-logo'>
+					<Link to='/'>Matt Parisien</Link>
+					<span className="copyright-symbol">©</span>
+				</div>
+			</ContainerFluid>
+		</StyledHeader>
 	);
 }
 
