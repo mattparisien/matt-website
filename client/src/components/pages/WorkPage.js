ac2ed47493244eefc96453d9cfe3cb6df8607023
Matt Parisien <matthewparisien4@gmail.com>
2022-03-10 15:16:02 -0500

    Add title to grid items

--- client/src/components/pages/WorkPage.js
+++ client/src/components/pages/WorkPage.js
@@ -1,14 +1,12 @@
-import { Box } from "@mui/material";
-import React, { useContext, useEffect, useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import React, { useContext, useState } from "react";
+import { useTheme } from "styled-components";
 import { DataContext } from "../../App/App";
+import { device } from "../../styles/breakpoints";
 import Layout from "../Containers/Layout";
 import Line from "../Divider/Line";
-import ResponsiveGrid from "../Grid/ResponsiveGrid";
 import UnorderedList from "../Lists/UnorderedList";
 import ParagraphLayout from "../Paragraph/ParagraphLayout";
-import { useMediaQuery } from "@mui/material";
-import { device } from "../../styles/breakpoints";
-import { useTheme } from "styled-components";
 
 function WorkPage() {
 	const theme = useTheme();
@@ -114,15 +112,16 @@ const ProjectsGrid = ({ items, tablet, theme }) => {
 
 	const itemWrapper = {
 		height: tablet ? "54vw" : "120vw",
-
 		gridColumn: tablet ? "span 3" : "span 6",
+		backgroundColor: theme.colors.blue,
 		padding: "2vw",
-		display: "flex",
-		alignItems: "center",
-		justifyContent: "center",
 		"&:hover .ProjectsGrid__image": {
 			opacity: 0,
 		},
+		"&:hover .info-bar-top, &:hover .info-bar-bottom": {
+			opacity: 1,
+			transform: 'translateY(0)'
+		}
 	};
 
 	const video = {
@@ -141,6 +140,30 @@ const ProjectsGrid = ({ items, tablet, theme }) => {
 		width: "100%",
 		backgroundColor: theme.colors.yellow,
 		overflow: "hidden",
+	};
+
+	const infoBarTop = {
+		position: "absolute",
+		left: 0,
+		top: 0,
+		color: theme.colors.light,
+		fontSize: '1rem',
+		transform: 'translateY(100%)',
+		opacity: 0,
+		transition: "400ms ease"
+	};
+
+	const infoBarBottom = {
+		position: "absolute",
+		right: 0,
+		bottom: 0,
+		color: theme.colors.light,
+		width: "50%",
+		fontSize: '1rem',
+		transform: 'translateY(100%)',
+		opacity: 0,
+		transition: "400ms ease",
+		transitionDelay: "100ms"
 	};
 
 	return (
@@ -162,23 +185,40 @@ const ProjectsGrid = ({ items, tablet, theme }) => {
 							rel='noreferrer'
 							key={project.id}
 						>
-							<Box className='ProjectsGrid__media-wrapper' sx={mediaWrapper}>
-								<Box
-									className='ProjectsGrid__image'
-									src={project.Cover.image.url}
-									alt={project.Cover.image.alt}
-									component={"img"}
-									sx={itemStyle}
-								></Box>
-								<video
-									style={video}
-									autoPlay
-									loop
-									playsInline
-									mute
-									key={project.id}
-									src={project.Cover.video.url}
-								></video>
+							<Box
+								className='ProjectGrid__item__inner'
+								sx={{
+									position: "relative",
+									width: "100%",
+									height: "100%",
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "center",
+									flexDirection: "column",
+								}}
+							>
+								<Box className='info-bar-top' sx={infoBarTop}>
+									<Box className='title'>{project.Title}</Box>
+								</Box>
+								<Box className='ProjectsGrid__media-wrapper' sx={mediaWrapper}>
+									<Box
+										className='ProjectsGrid__image'
+										src={project.Cover.image.url}
+										alt={project.Cover.image.alt}
+										component={"img"}
+										sx={itemStyle}
+									></Box>
+									<video
+										style={video}
+										autoPlay
+										loop
+										playsInline
+										mute
+										key={project.id}
+										src={project.Cover.video.url}
+									></video>
+								</Box>
+								<Box className='info-bar-bottom' sx={infoBarBottom}>{project.PreviewText}</Box>
 							</Box>
 						</Box>
 					);
