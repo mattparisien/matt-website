891416ab6ce8bb439ac765516d3b4b80ead880b6
Matt Parisien <matthewparisien4@gmail.com>
2022-03-13 18:05:06 -0400

    Add cursor delay

--- client/src/components/CursorFollower/CursorFollower.js
+++ client/src/components/CursorFollower/CursorFollower.js
@@ -1,18 +1,26 @@
 import React, { useEffect, useRef } from "react";
 import useMouseMove from "../../helpers/hooks/useMouseMove";
+import $ from "jquery";
 
 function CursorFollower() {
 	const [location] = useMouseMove();
 	const follower = useRef(null);
 
 	useEffect(() => {
-		// function lerp(start, end, t) {
-		// 	return start * (1 - t) + end * t;
-		// }
+		function lerp(start, end, t) {
+			return start * (1 - t) + end * t;
+		}
 
-		// if (follower.current) {
-		// 	const lerp = (a, b, n) => (1 - n) * a + n * b;
-		// }
+		if (follower.current) {
+			const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+			setTimeout(() => {
+				$(follower.current).css({
+					left: location.pageX,
+					top: location.pageY,
+				});
+			}, 100);
+		}
 	}, [location, follower]);
 
 	return (
